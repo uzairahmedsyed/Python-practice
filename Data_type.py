ce04c6c78c06b060ae501f515e489b724be6f773
UZAIR AHMED <uzairsyed2024@gmail.com>
2025-10-26 02:49:36 +0500

    file upload

---
+++ Data_type.py
@@ -0,0 +1,96 @@
+# -------------------------STRING TYPE ---------------
+
+# a = "My name is "
+# b = "UZAIR"
+# print (a+b) 
+
+
+
+# --------------------INT TYPE-----------------
+
+# a = 10
+# b = 20
+# c = a+ b
+# print("The answer is :", c)
+
+
+
+# -------------------- TYPE CASTING----------------------
+
+
+# converting string to int and then add 5 to it.
+
+
+# x = "25"    # direct x = int("25") kar k bhi int type mein convert krskte hain
+# y = int(x)
+# z = 5 
+
+# add = y + z
+
+# print(add)
+# print(type(add))
+
+
+# converting int to string and then concatenate it the "APPLE".
+
+
+# y = 10
+# x = str(y)
+# z = "apple"
+
+# concatenate = x + z
+
+# print(concatenate)
+# print(type(concatenate))
+
+
+
+# convert float to int 
+
+
+# z = 4.7
+# y = int(z) # convert krte hue float se int mein value int-floor ki taraf ati (na k ceiling ki)
+
+# print(y)
+# print(type(y))
+
+
+
+#  a = True ko integer mein convert karo aur print karo.
+
+
+# a = True
+# # print(a)
+# # print(type(a))
+
+# b = int(a) # true integer mein convert hone per is ki value 1 hoti hai
+
+# print(b)
+# print(type(b))
+
+
+
+# Ek variable num = 0 hai. integer se boolean mein convert karo — kya output aayega?
+
+
+# num = -8.89
+# convert_to_boolean = bool(num)
+# print(convert_to_boolean)
+# print(type(convert_to_boolean))
+
+
+#  try ko convert interger/string to int  (and try with help of try and except )
+
+# string_num = "100abc"
+# num = int(string_num)
+# print(string_num)
+# print(type(string_num))
+# print(num)
+# print(type(num))
+
+# # try:
+# #     converted_num = int(string_num)
+# #     print("converted number : ", converted_num)
+
+# # except ValueError:
+# #     print("Conversion failed")
