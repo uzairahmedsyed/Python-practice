9f1702da45c58b4e603ad84dc524552f1ea0d3ba
UZAIR AHMED <uzairsyed2024@gmail.com>
2025-11-13 02:55:52 +0500

    while loop basic practice

---
+++ loops practice.py
@@ -0,0 +1,75 @@
+
+##### ===============  13/11/25 ====================================================== ##########
+
+
+### Question 1: Print numbers from 1 to 100. 
+
+
+# asc_count = 1
+
+# while asc_count <= 100:
+#     print(f"count is: {asc_count}")
+#     asc_count += 1
+
+
+
+
+
+
+
+### Question 2: Print numbers from 100 to 1.
+
+
+# desc_count = 100
+
+# while desc_count >= 1:
+#     print(f"count is: {desc_count}")
+#     desc_count -= 1
+
+
+
+
+
+
+
+
+### Question 3: Print the multiplication table of a number n. 
+
+
+# n = 3 
+# m = 1
+# while m <= 5: 
+#     o = n * m
+#     print(f"{n} * {m} = {o} ")
+#     # print(n*m)
+#     m += 1 
+
+
+
+
+
+
+
+
+
+### Question 4: Print the elements of the following list using a loop: [1, 4, 9, 16, 25, 36, 49, 64, 81,100] 
+
+
+# list_of_numbers = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+# index_of_list = 0
+
+# print(len(list_of_numbers))
+
+# while index_of_list < len(list_of_numbers):
+
+#     print(f"list of number is : {list_of_numbers[index_of_list]}")
+#     print(f"index_of_list before add 1 =  {index_of_list}")
+
+#     index_of_list += 1 
+
+#     print(f"index_of_list after add 1 =  {index_of_list}")
+#     print("=========================================================")
+
+
+
+###===========================================================##############
